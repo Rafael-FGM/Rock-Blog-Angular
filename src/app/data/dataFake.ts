6c4f8cd39f0088e4f225d03239e5a7e6d042734f
Rafael Ferreira <rafamota2@hotmail.com>
2023-10-04 20:46:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Homem de Ferro em 3D!",
+        "description": "Marvel anuncia novo filme do Homem de Ferro, confira! ",
+        "photoCover": "https://wallpapers.com/images/hd/iron-man-pictures-k83lqhe6vnh95b1m.jpg"
+    },
+
+    {
+        "id": "2",
+        "title": "Nova série anunciada!",
+        "description": "A série chega com tudo e promete chocar os telespectadores!",
+        "photoCover": "https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2022/10/legiao_D8mFjTNqpZY5.png.webp"
+    },
+
+    {
+        "id": "3",
+        "title": "A história da Marvel Studios!",
+        "description": "Marvel desde o início. Histórias inéditas! ",
+        "photoCover": "https://segredosdomundo.r7.com/wp-content/uploads/2020/06/marvel-a-historia-da-editora-nos-quadrinhos-e-no-cinema.jpg"
+    },
+
+    {
+        "id": "4",
+        "title": "Lançamento Marvel - WAKANDA FOREVER!",
+        "description": "O filme mais esperado do ano! Confira os bastidores!",
+        "photoCover": "https://hollywoodforevertv.com.br/media/uploads/lancamentos-da-marvel-em-2022.jpg"
+    }
+]
